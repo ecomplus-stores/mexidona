78a1a69ab034515c976149ca034f00ce0f24449f
Leonardo Matos <leomp120894@gmail.com>
2023-06-05 18:15:40 -0300

    Fixing hidden prices customizations

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,9 +4,7 @@ module.exports = () => ({
   resolve: {
     alias: {
         './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
-        './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
-        './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
-        './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js')
+        './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html')
     }
   }
 })
